1fa8c5fad6699aa4a083251a77d3e3a08ea7815f
Duy Nhut <nhutb62019@gmail.com>
2025-04-01 00:16:59 +0700

    Fe sprint1

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="bg-indigo-600 text-white px-6 py-4 shadow-lg flex justify-between items-center">
+      <h1 className="text-xl font-bold">SpeakPro</h1>
+      <div className="space-x-4">
+        <Link to="/" className="hover:text-gray-300">Home</Link>
+        <Link to="/signin" className="hover:text-gray-300">Sign In</Link>
+        <Link to="/signup" className="hover:text-gray-300">Sign Up</Link>
+        <Link to="/reset-password" className="hover:text-gray-300">Reset Password</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
